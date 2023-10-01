a41719ecf8e7e4aabc5ca776ce4e54807fd457a6
Mehrdad Rahidi <rashidi.mehrdad@gmail.cm>
2023-10-01 15:49:47 +0330

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -32,6 +32,19 @@ class Item(Resource):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'Item deleted'}
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('price', type=float, required=True, help="This field cannot be left blank!")
+        data = parser.parse_args()
+        # data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 
 class ItemList(Resource):
